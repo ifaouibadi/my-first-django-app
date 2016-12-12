b24dc0d411bfc9c203b9b100fc028dbd27093f79
badi ifaoui <badi1wow@gmail.com>
2016-12-12 12:20:13 +0100

    added: newsletter model

--- newsletter/admin.py
+++ newsletter/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from newsletter.models import SignUp
+
+
+class SignUpAdmin(admin.ModelAdmin):
+    list_display = ["email", "full_name", "created_at", "updated_at"]
+
+    class Meta:
+        model = SignUp
+
+
+admin.site.register(SignUp, SignUpAdmin)
--- newsletter/models.py
+++ newsletter/models.py
@@ -2,4 +2,13 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
 # Create your models here.
+class SignUp(models.Model):
+    email = models.EmailField()
+    full_name = models.CharField(blank=True, null=True, max_length=120)
+    created_at = models.DateTimeField(auto_now_add=True, auto_now=False)
+    updated_at = models.DateTimeField(auto_now_add=False, auto_now=True)
+
+    def __str__(self):
+        return self.email
